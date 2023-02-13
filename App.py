3302308904e51ab99aaa0395783403aa9fe12bd4
John030903 <nguyenhoangkhanhduy030903@gmail.com>
2023-02-13 19:55:41 +0700

    create app

---
+++ App.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import requests
+import json
+
+def use_api(url):
+    """
+    It takes a URL as an argument, makes a request to that URL, and returns the response as a JSON
+    object
+    
+    :param url: The URL of the API you want to use
+    :return: A dictionary
+    """
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70',}
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.content)
+    return data
+
+data  = use_api("http://10.12.140.181:5000")
+st.write(data)
